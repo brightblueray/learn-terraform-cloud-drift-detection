b3968b3ed5904818cf4a72ab17d0a7120727c7cb
Ray Ryjewski <brightblueray@users.noreply.github.com>
2023-07-14 16:48:39 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ provider "aws" {
 
   default_tags {
     tags = {
-      Workspace = "learn-terraform-cloud-drift-detection"
-      TTL = "7/7/2023"
-      Environment = "Demo"
-      IAC = "Managed by Terraform Cloud"
-      Name = "CH Demo"
-      Project = "CH-IAC-1234"
+      Workspace     = "learn-terraform-cloud-drift-detection"
+      TTL           = "7/7/2023"
+      Environment   = "Demo"
+      IAC           = "Managed by Terraform Cloud"
+      Name          = "CH Demo"
+      Project       = "CH-IAC-1234"
       "Cost Center" = "CH-ITO"
     }
   }
@@ -24,7 +24,7 @@ data "aws_availability_zones" "available" {
 }
 
 data "aws_key_pair" "example" {
-  key_name           = "rkr-key"
+  key_name = "rkr-key"
   # include_public_key = true
 
   # filter {
@@ -53,13 +53,21 @@ resource "aws_security_group" "bastion" {
   name   = "bastion_ssh"
   vpc_id = module.vpc.vpc_id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    # THIS DOES NOT INCLUDE MY IP ADDRESS, but now it does
-    cidr_blocks = ["65.60.165.105/32"]
-  }
+  # ingress = [
+  #   { from_port = 22
+  #     to_port   = 22
+  #     protocol  = "tcp"
+  #     # THIS DOES NOT INCLUDE MY IP ADDRESS
+  #     cidr_blocks = ["65.60.165.105/32"]
+  #   },
+  #   {
+  #     from_port = 8000
+  #     to_port   = 8000
+  #     protocol  = "tcp"
+  #     # THIS DOES NOT INCLUDE MY IP ADDRESS
+  #     cidr_blocks = ["65.60.165.105/32"]
+  #   },
+  # ]
 
   egress {
     from_port   = 0
@@ -68,6 +76,25 @@ resource "aws_security_group" "bastion" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group_rule" "ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = ["65.60.165.105/32"]
+  security_group_id = aws_security_group.bastion.id
+}
+
+resource "aws_security_group_rule" "ak" {
+  type              = "ingress"
+  from_port         = 8000
+  to_port           = 8000
+  protocol          = "tcp"
+  cidr_blocks = ["65.60.165.105/32"]
+  security_group_id = aws_security_group.bastion.id
+}
+
 
 data "aws_ami" "amazon_linux" {
   most_recent = true
@@ -85,5 +112,6 @@ resource "aws_instance" "bastion" {
 
   subnet_id              = module.vpc.public_subnets[0]
   vpc_security_group_ids = [aws_security_group.bastion.id]
-  key_name = data.aws_key_pair.example.key_name
+  key_name               = data.aws_key_pair.example.key_name
+  iam_instance_profile = "rryjewski-test-role"
 }
